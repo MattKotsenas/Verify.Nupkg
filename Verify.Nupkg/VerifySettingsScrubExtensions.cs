b754468781b82fc0a2acaefbc248139478725954
Simon Cropp <simon.cropp@gmail.com>
2024-02-03 21:00:03 +1100

    add missing fluent apis

--- Verify.Nupkg/VerifySettingsScrubExtensions.cs
+++ Verify.Nupkg/VerifySettingsScrubExtensions.cs
@@ -22,6 +22,23 @@ public static class VerifySettingsScrubExtensions
     }
 
     /// <summary>
+    /// Scrub the nuspec file of common changes.
+    /// </summary>
+    /// <param name="settings">
+    /// The <see cref="VerifySettings"/> to modify.
+    /// </param>
+    /// <remarks>
+    /// Sources of common changes:
+    /// - Package version
+    /// - Commit hash
+    /// </remarks>
+    public static SettingsTask ScrubNuspec(this SettingsTask settings)
+    {
+        settings.CurrentSettings.ScrubNuspec();
+        return settings;
+    }
+
+    /// <summary>
     /// Scrub the version from the nuspec file.
     /// </summary>
     /// <param name="settings">
@@ -37,6 +54,22 @@ public static class VerifySettingsScrubExtensions
     }
 
     /// <summary>
+    /// Scrub the version from the nuspec file.
+    /// </summary>
+    /// <param name="settings">
+    /// The <see cref="VerifierSettings"/> to modify.
+    /// </param>
+    /// <remarks>
+    /// In some scenarios, package versions are frequent and obvious changes. In these cases, scrub the version
+    /// to reduce the noise in the diff.
+    /// </remarks>
+    public static SettingsTask ScrubNuspecVersion(this SettingsTask settings)
+    {
+        settings.CurrentSettings.ScrubNuspecVersion();
+        return settings;
+    }
+
+    /// <summary>
     /// Scrub the commit info from the nuspec file.
     /// </summary>
     /// <param name="settings">
@@ -49,5 +82,21 @@ public static class VerifySettingsScrubExtensions
     public static void ScrubNuspecCommit(this VerifySettings settings)
     {
         settings.ScrubLinesWithReplace(extension: "nuspec", line => _scrubber.ScrubCommit(line));
+    }
+
+    /// <summary>
+    /// Scrub the commit info from the nuspec file.
+    /// </summary>
+    /// <param name="settings">
+    /// The <see cref="VerifierSettings"/> to modify.
+    /// </param>
+    /// <remarks>
+    /// In some scenarios, the commit used in the package are frequent and obvious changes. In these cases, scrub the commit
+    /// to reduce the noise in the diff.
+    /// </remarks>
+    public static SettingsTask ScrubNuspecCommit(this SettingsTask settings)
+    {
+         settings.CurrentSettings.ScrubNuspecCommit();
+         return settings;
     }
 }
