1f29b29704a0a8ba361dfc644b325a9692607899
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-02-11 21:49:00 -0800

    Add benchmark

---
+++ benchmarks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+
+namespace Verify.Benchmarks;
+
+public class Md5VsSha256
+{
+    private const int N = 10000;
+    private readonly byte[] data;
+
+    private readonly SHA256 sha256 = SHA256.Create();
+    private readonly MD5 md5 = MD5.Create();
+
+    public Md5VsSha256()
+    {
+        data = new byte[N];
+        new Random(42).NextBytes(data);
+    }
+
+    [Benchmark]
+    public byte[] Sha256() => sha256.ComputeHash(data);
+
+    [Benchmark]
+    public byte[] Md5() => md5.ComputeHash(data);
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = DefaultConfig.Instance
+            .AddDiagnoser(MemoryDiagnoser.Default)
+            .AddDiagnoser(ExceptionDiagnoser.Default)
+            .AddJob(Job
+                 .ShortRun
+                 .WithLaunchCount(1)
+                 .WithToolchain(InProcessNoEmitToolchain.Instance)); // Avoid Defender
+
+        var summary = BenchmarkRunner.Run<Md5VsSha256>(config);
+    }
+}
