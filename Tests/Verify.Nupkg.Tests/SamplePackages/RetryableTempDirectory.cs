86abc2f42a49ce31d8f9cd811086aacf8644b11e
Matt Kotsenas <Matt.Kotsenas@gmail.com>
2024-02-21 17:32:44 -0800

    Add retry to temp directory deletion (#9)
    Antivirus (I _believe_) is causing occasional flakiness on dev machines where the file handle is being held open after the build completes, which causes an `IOException` when the temporary working directory is deleted.
    
    (Try to) avoid those issues by creating a custom retriable directory deletion `IDisposable`.

---
+++ Tests/Verify.Nupkg.Tests/SamplePackages/RetryableTempDirectory.cs
@@ -0,0 +1,48 @@
+﻿using System.IO.Abstractions;
+
+namespace Verify.Nupkg.Tests;
+
+/// <summary>
+/// Helper class that retries deleting a temp directory multiple times. This avoid flakiness when running tests
+/// due to anti-virus or other processes locking the directory.
+/// </summary>
+internal class RetryableTempDirectory : DisposableDirectory
+{
+    public RetryableTempDirectory(IDirectoryInfo directoryInfo) : base(directoryInfo)
+    {
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        IOException? lastException = null;
+
+        for (int i = 0; i < 3; i++)
+        {
+            try
+            {
+                base.Dispose(disposing);
+                return;
+            }
+            catch (IOException ex)
+            {
+                lastException = ex;
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        if (lastException is not null)
+        {
+            throw new IOException("Failed to delete temp directory after multiple retries.", lastException);
+        }
+    }
+
+    // Once https://github.com/TestableIO/System.IO.Abstractions.Extensions/pull/63 lands, remove this method
+    // and use the updated TestableIO.Abstractions.Extensions package instead.
+    internal static string GetRandomTempPath()
+    {
+        var temp = Path.GetTempPath();
+        var fileName = Path.GetRandomFileName();
+        return Path.Combine(temp, fileName);
+    }
+}
