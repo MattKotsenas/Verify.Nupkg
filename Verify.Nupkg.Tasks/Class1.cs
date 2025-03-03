36ea75a6e1c3bfbda399f152398597e60200db61
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-03-03 12:13:15 -0800

    WIP

---
+++ Verify.Nupkg.Tasks/Class1.cs
@@ -0,0 +1,83 @@
+﻿﻿using Microsoft.Build.Framework;
+
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace Verify.Nupkg.Tasks;
+
+public class VerifyNuspec : Task
+{
+    /// <summary>
+    /// </summary>
+    [Required]
+    public string NuspecFile { get; set; }
+
+    /// <summary>
+    /// Create a baseline of Roslyn diagnostics from a given SARIF log file.
+    /// </summary>
+    /// <returns><see langword="true" />, if successful</returns>
+    public override bool Execute()
+    {
+        if (ErrorLog is null)
+        {
+            LogWarning(warningCode: DiagnosticIds.Sarif.NotSpecified, "ErrorLog property not specified");
+            return true;
+        }
+
+        if (!File.Exists(ErrorLog))
+        {
+            LogWarning(warningCode: DiagnosticIds.Sarif.NotFound, "SARIF log file not found: {0}", ErrorLog);
+            return true;
+        }
+
+        string baselineFile = ValidateBaselineFile(BaselineFiles);
+
+        try
+        {
+            using Stream stream = File.OpenRead(ErrorLog!);
+
+            IReadOnlyCollection<DiagnosticConfig> configs = _parser.Parse(stream);
+
+            string newBaseline = _serializer.Serialize(configs);
+
+            if (AreDifferent(baselineFile, newBaseline))
+            {
+                if (AutoBaseline)
+                {
+                    _writer.Write(baselineFile, newBaseline);
+                }
+                else
+                {
+                    LogWarning(warningCode: DiagnosticIds.Baseline.Mismatch, "Baseline mismatch: {0}", baselineFile);
+                }
+            }
+
+            return true;
+        }
+        catch (UnsupportedVersionException)
+        {
+            LogWarning(warningCode: DiagnosticIds.Sarif.V1Format, "SARIF log '{0}' is in v1 format; SquiggleCop requires SARIF v2.1 logs", ErrorLog);
+            return true;
+        }
+    }
+
+    private string ValidateBaselineFile(ITaskItem[] baselineFiles)
+    {
+        if (baselineFiles.Length > 1)
+        {
+            LogWarning(DiagnosticIds.Baseline.AmbiguousReference, "Multiple baseline files found in @(AdditionalFiles); using the first one found: {0}", baselineFiles[0]);
+        }
+
+        return baselineFiles[0].ItemSpec;
+    }
+
+    private bool AreDifferent(string path, string contents)
+    {
+        return !File.Exists(path) || _differ.Diff(File.ReadAllText(path), contents).HasDifferences;
+    }
+
+    private void LogWarning(string warningCode, string message, params object[] messageArgs)
+    {
+        string helpLink = $"https://github.com/MattKotsenas/SquiggleCop/tree/{ThisAssembly.GitCommitId}/docs/{warningCode}.md";
+        Log.LogWarning(subcategory: null, warningCode, helpKeyword: null, helpLink, file: null, lineNumber: 0, columnNumber: 0, endLineNumber: 0, endColumnNumber: 0, message, messageArgs);
+    }
+}
