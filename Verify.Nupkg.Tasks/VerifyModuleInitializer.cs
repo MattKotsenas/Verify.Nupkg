ea53dba42d68e601026526ef70f56789ca82c404
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-03-24 21:19:43 -0700

    WIP

--- Verify.Nupkg.Tasks/VerifyModuleInitializer.cs
+++ Verify.Nupkg.Tasks/VerifyModuleInitializer.cs
@@ -2,10 +2,10 @@
 
 namespace Verify.Nupkg.Tasks;
 
-public class VerifyModuleInitializer
-{
-#pragma warning disable CA2255 // The ModuleInitializer attribute should not be used in libraries
-    [ModuleInitializer] // Required by Verify
-#pragma warning restore CA2255 // The ModuleInitializer attribute should not be used in libraries
-    public static void Initialize() => VerifyNupkg.Initialize();
-}
+//public class VerifyModuleInitializer
+//{
+//#pragma warning disable CA2255 // The ModuleInitializer attribute should not be used in libraries
+//    [ModuleInitializer] // Required by Verify
+//#pragma warning restore CA2255 // The ModuleInitializer attribute should not be used in libraries
+//    public static void Initialize() => VerifyNupkg.Initialize();
+//}
